e9dc02b4a3b401ad4e3efcb55c4e79aa2d7fc106
TheS1mon <s1monp@mailbox.org>
2025-01-14 23:06:37 +0100

    Fixed after_install user switching bug

--- after_install.sh
+++ after_install.sh
@@ -26,8 +26,15 @@ passwd ${username}
 ## Disable root login
 # passwd -l root
 
+## Copy the rest of the script to the users directory
+
+cat <<EOF > /home/${username}/continue.sh
+#!/usr/bin/env bash
+set-e # Breche direkt ab, wenn ein Fehler auftritt
+
+cd ~/
+
 ## Setup Oh-My-Zsh
-su ${username}
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 sed -i '/zstyle .*mode auto/ s/^#\s*//' ~/.zshrc
 sed -i 's/^#\s*HIST_STAMPS="mm\/dd\/yyyy"/HIST_STAMPS="dd.mm.yyyy"/' ~/.zshrc
@@ -35,3 +42,17 @@ echo "export EDITOR='nvim'" >> ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sed -i '/^plugins=(/ s/)/ colored-man-pages zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+
+rm .zshrc.pre-oh-my-zsh
+
+EOF
+
+## Create an empty zsh config file so the first run init do not appear when changing user
+echo "# This is a dump comment" > /home/${username}/.zshrc
+## Set up environment for script continuation
+chown ${username}:${username} /home/${username}/.zshrc
+chown ${username}:${username} /home/${username}/continue.sh
+chmod +x /home/${username}/continue.sh
+
+echo "Please run the script continue.sh"
+su ${username}
